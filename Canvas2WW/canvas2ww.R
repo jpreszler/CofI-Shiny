415c8452f52ba5f5cf94acb3fb5fe5146bdb1bfb
Jason Preszler <jpreszler@gmail.com>
2018-07-18 09:18:23 -0600

    R script for canvas2ww since deployment is a FERPA problem

---
+++ Canvas2WW/canvas2ww.R
@@ -0,0 +1,22 @@
+#This is a function that does the work of the Canvas2WW app for
+#use in R directly since cloud deployment of the app is a FERPA 
+#problem.
+
+canvas2ww <- function(filename){
+  inDF <- read.csv(filename, header=TRUE) %>% 
+    select(Student, SIS.User.ID, SIS.Login.ID, Section) %>% 
+    filter(!is.na(SIS.User.ID))
+  #clean and manipulate
+  inDF$First <- str_split(inDF$Student, " ", n=2, simplify = TRUE)[,1]
+  inDF$Last <- str_split(inDF$Student, " ", n=2, simplify = TRUE)[,2]
+  inDF$blank1 <- " "
+  inDF$blank2 <- " "
+  inDF$C <- "C"
+  inDF$username <- str_split(inDF$SIS.Login.ID, "@", n=2, simplify=TRUE)[,1]
+  inDF <- select(inDF,SIS.User.ID, Last, First, C, blank1, Section, blank2, SIS.Login.ID, username )
+  
+  #write inDF to file
+  outFile <- paste(basename(filename),'lst', sep = ".")
+  write.table(inDF,outFile, row.names = FALSE, col.names = FALSE, quote = FALSE, sep=",")
+  
+}
