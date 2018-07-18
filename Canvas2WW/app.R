004b99c4149cb048cb021ce08ce3e59be1929fd6
Jason Preszler <jpreszler@gmail.com>
2018-07-17 21:47:51 -0600

    started canvas to ww roster conversion app - upload/download working

---
+++ Canvas2WW/app.R
@@ -0,0 +1,49 @@
+library(shiny)
+library(dplyr)
+library(stringr)
+library(tidyr)
+library(markdown)
+
+ui <- fluidPage(
+   
+   titlePanel("Canvas to WebWork Roster Converter"),
+   
+   sidebarLayout(
+      sidebarPanel(
+        div(id = "description", includeMarkdown("instructions.md"))
+        
+      ),
+      
+      mainPanel(
+        fileInput("canvas", "Choose Canvas csv roster", 
+                  multiple=FALSE, accept = c(
+                      "text/csv",
+                      "text/comma-separated-values,text/plain",
+                      ".csv")),
+        downloadButton('download_data', 'Download WW')
+      )
+   )
+)
+
+server <- function(input, output) {
+  
+  output$download_data <- downloadHandler(
+    filename = function() {
+        req(input$canvas)
+        paste('WW-',input$canvas$name,sep='')
+    },
+    content = function(file) {
+      req(input$canvas)
+      inDF <- read.csv(input$canvas$datapath, header=TRUE)
+      #clean and manipulate
+      
+      
+      #write inDF to file
+      write.csv(inDF,file)
+    }
+  )
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
---
+++ Canvas2WW/instructions.md
@@ -0,0 +1,8 @@
+## Directions
+
+This app takes an uploaded canvas roster and creates a csv file for populating WebWork courses.
+
+### Exporting Canvas Roster
+1.
+2.
+3.
