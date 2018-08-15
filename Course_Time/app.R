089739766921772e749f9a9f35b2510187f08e26
Jason Preszler <jpreszler@gmail.com>
2018-08-15 13:10:47 -0600

    removed smoothing, added building selection

--- Course_Time/app.R
+++ Course_Time/app.R
@@ -17,15 +17,17 @@ library(stringr)
 
 #load IR data, from Teagle-cleaned-flat-non-blank, merged with current(18-19)
 #catalog and basic mutate done from hist3
-dump <- read.csv("course-time-data.csv")
+dump <- read.csv("course-time-data.csv") %>% rename(Dept=`Dept.`)
 
 #the main data processing function
 library(scales)
-tgDF <- function(yrStr){
+tgDF <- function(yrStr, bldStr){
   if(yrStr=="All")
     dumpF <- dump
   else
     dumpF <- filter(dump, Year %in% yrStr)
+  if(bldStr!="All")
+    dumpF <- filter(dumpF, Bldg %in% bldStr)
   return(
     select(dumpF, Crs.Name, Division, Semester, Days, Start.Time, 
              End.Time) %>%
@@ -48,7 +50,8 @@ tgDF <- function(yrStr){
 timeGraph <- function(tgDF, dayStr){
   filter(tgDF, Days %in% dayStr) %>%
     ggplot(aes(x=time, y=cumsum(ifelse(SE=="Start.Time", crs.cnt, -1*crs.cnt)), col=Division)) + 
-    geom_smooth(se=FALSE)+ facet_wrap(~Semester) +
+    geom_line(size=2)+#geom_smooth(se=TRUE,n=n, span=spStr)+ 
+    facet_wrap(~Semester) +
     scale_x_datetime(breaks = date_breaks("2 hour"),labels=date_format("%I:%M", tz="America/Boise"))+
     ylab("Number of Courses")+ggtitle("Number of Classes by Division and Semester")
 }
@@ -69,7 +72,11 @@ ui <- fluidPage(
                            selected = "MWF"),
         checkboxGroupInput("Years", label = h3("Years to Include"),
                            choices = as.list(c("All", sort(unique(dump$Year)))),
+                           selected = "All"),
+        checkboxGroupInput("Buildings", label = h3("Classes Meeting in:"),
+                           choices = as.list(c("All", sort(unique(as.character(dump$Bldg))))),
                            selected = "All")
+        
         
       ),
       
@@ -83,7 +90,7 @@ ui <- fluidPage(
 server <- function(input, output) {
    
    output$distPlot <- renderPlot({
-     tg <- tgDF(input$Years)
+     tg <- tgDF(input$Years, input$Buildings)
      tg <- tg[order(tg$time),]
      timeGraph(tg, input$dayStr)
    })
